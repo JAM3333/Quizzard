1dd2837c569f7343284741b2b17e9ad6e5d63dd0
JAM <jonas.muxel@student.htldornbirn.at>
2024-01-10 09:15:52 +0100

    FileUpldoad

--- Quizzard/src/components/FuncTest.vue
+++ Quizzard/src/components/FuncTest.vue
@@ -1,16 +1,40 @@
+<script setup>
+  import FileManager from "../classes/file.js";
+</script>
+
+
 <template>
   <v-container class="align-left">
-    <v-btn color="primary" @click="addQuestion">Add Question</v-btn>
-
+    <v-btn color="primary" @click="AddFile">Add File</v-btn>
+    <v-file-input clearable label="File input" variant="outlined" multiple show-size @change="ReadFiles" ref="fileUpload"></v-file-input>
   </v-container>
 </template>
 
 <script>
-  export default {
-  name: 'FuncTest',
-  props: {
-  },
+var fileManager = new FileManager();
+export default {
+  data: () => ({
+    fileData: [],
+    customInstructions: "",
+  }),
   methods: {
-  },
-  };
+    ReadFiles(){
+      const uploadedFiles = this.$refs.fileUpload.files;
+      if (uploadedFiles.length > 0){
+        console.log(uploadedFiles[0])
+        this.fileData = [];
+        for (let i = 0; i < uploadedFiles.length;i++){
+          var filetype = 
+          console.log(uploadedFiles[i].name);
+          var reader = new FileReader();
+          reader.readAsText(uploadedFiles[i]);
+          reader.onload = () => {
+            this.fileData.push([uploadedFiles[i].name,reader.result]);
+          }
+        }
+        console.log(this.fileData)
+      }
+    },
+  } 
+};
 </script>
