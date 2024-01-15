0503db0ae89bb5e803e04fa3054d137193137c31
Sodentix <fabianlampert9@gmail.com>
2024-01-15 12:11:30 +0100

    DatabaseSchematic

--- Quizzard/src/components/FuncTest.vue
+++ Quizzard/src/components/FuncTest.vue
@@ -1,16 +1,31 @@
 <template>
   <v-container class="align-left">
     <v-btn color="primary" @click="addQuestion">Add Question</v-btn>
-
+    <v-file-input
+      v-model="files"
+      label="Choose file(s)"
+      multiple
+      show-size
+    ></v-file-input>
+    <v-btn>Upload</v-btn>
   </v-container>
+
+    
+  
 </template>
 
 <script>
-  export default {
+export default {
   name: 'FuncTest',
-  props: {
-  },
+   data() {
+    return {
+      files: [] // Hier werden die hochgeladenen Dateien gespeichert
+    };
+   },
   methods: {
-  },
-  };
+    addQuestion() {
+      // Hier könnte Logik für das Hinzufügen einer Frage stehen
+    },
+  }
+};
 </script>
