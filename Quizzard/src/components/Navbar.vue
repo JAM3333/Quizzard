59faa2daf6a80cbadc92d79d2a4ced13772df641
KptnFishy <nils.miessgang@gmail.com>
2024-02-28 10:50:48 +0100

    Update Navbar.vue

--- Quizzard/src/components/Navbar.vue
+++ Quizzard/src/components/Navbar.vue
@@ -2,6 +2,7 @@
       <v-navigation-drawer
         expand-on-hover
         rail
+        style="position: fixed;"
       >
         <v-list density="compact" nav>
           <v-list-item :to="{path: '/home'}"
@@ -11,19 +12,20 @@
         </v-list>
 
         <v-divider></v-divider>
-
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-plus-circle-outline" title="Create Quiz" value="CreateQuiz" :to="{path: '/quizcreator'}"></v-list-item>
+<!-- Website for icons: https://pictogrammers.com/library/mdi/ -->
+        <v-list density="compact" class="d-flex justify space-between flex-column" nav>
+            <v-list-item prepend-icon="mdi-plus-circle-outline" title="Create Quiz" value="CreateQuiz" :to="{path: '/quizcreator'}"></v-list-item>
+          <v-list-item prepend-icon="mdi-magnify" title="Discover" value="discover"></v-list-item>
           <v-list-item prepend-icon="mdi-folder" title="My Quizzes" value="myQuiz"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Fwiends" value="friends"></v-list-item>
           <v-divider></v-divider>
+         <!-- <v-icon style="position: absolute; bottom: 10px;">mdi-logout</v-icon> -->
         </v-list>
-        <v-list density="compact" nav class="align-end">
-          <v-list-item prepend-icon="mdi-account" title="Fwiends" class="align-end"></v-list-item>
-        <!-- Website for icons: https://pictogrammers.com/library/mdi/ -->
-        </v-list>
+        <template v-slot:append>
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-account" title="Username" value="Account"></v-list-item>
+          </v-list>
+        </template>
       </v-navigation-drawer>
-
 </template>
 
 <script>
