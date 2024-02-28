222804ff01e42737891378b33710281bbbc6ff8d
JAM <jonas.muxel@student.htldornbirn.at>
2024-02-28 10:46:58 +0100

    Load Data from index.js api

--- Quizzard/src/components/QuestionCard.vue
+++ Quizzard/src/components/QuestionCard.vue
@@ -1,16 +1,10 @@
 <template>
-  <v-card>
-  <v-card-title>{{ quiz.title }}</v-card-title>
-  <v-card-text>
-  <div v-for="(question, index) in quiz.questions" :key="index">
-          {{ question }}
-  </div>
-  </v-card-text>
-  <v-card-actions>
-  <v-btn color="primary" @click="addQuestion">Add Question</v-btn>
-  <v-btn color="error" @click="deleteQuiz">Delete Quiz</v-btn>
-  </v-card-actions>
-  </v-card>
+   <v-card width="15vw" color="secondary" height="30vh" class="d-flex align-center flex-column ml-8" elevation="12">   
+      <v-toolbar color="primary" class="text-h1" :title="name"> 
+        <v-btn icon="mdi-play"></v-btn>
+        <v-btn icon="mdi-wrench"></v-btn>
+      </v-toolbar>
+    </v-card>
   </template>
    
   <script>
@@ -18,14 +12,6 @@
   name: 'QuizCard',
   props: {
       quiz: Object,
-  },
-  methods: {
-      addQuestion() {
-        this.quiz.questions.push('New Question');
-      },
-      deleteQuiz() {
-        this.$emit('delete');
-      },
   },
   };
   </script>
