76684c4e8dfb5ce05fc4c1543260cff8f22a631d
JAM <jonas.muxel@student.htldornbirn.at>
2024-03-01 10:24:10 +0100

    New JSON

--- Quizzard/src/components/QuestionCard.vue
+++ Quizzard/src/components/QuestionCard.vue
@@ -1,10 +1,16 @@
 <template>
    <v-card width="75vw" color="secondary" height="30vh" class="d-flex align-center flex-column ml-8" elevation="12">   
-    <v-toolbar
-      class="text-medium-emphasis font-weight-light"
-      color="secondary"
-      :title="question"
-    ></v-toolbar>  
+    <v-expansion-panels>
+      <v-expansion-panel
+            expand
+            title="Custom Instructions"
+            color="primary"
+          >
+            <v-expansion-panel-text>
+              <v-textarea label="Your instructions"></v-textarea>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
   </v-card>
   </template>
    
