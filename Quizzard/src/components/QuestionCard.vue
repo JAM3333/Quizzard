3f94476ce232ae7e809a023c7c2fe3a7d805d0d7
JAM <jonas.muxel@student.htldornbirn.at>
2024-03-01 10:04:24 +0100

    Added basic question list

--- Quizzard/src/components/QuestionCard.vue
+++ Quizzard/src/components/QuestionCard.vue
@@ -1,17 +1,32 @@
 <template>
-   <v-card width="15vw" color="secondary" height="30vh" class="d-flex align-center flex-column ml-8" elevation="12">   
-      <v-toolbar color="primary" class="text-h1" :title="name"> 
-        <v-btn icon="mdi-play"></v-btn>
-        <v-btn icon="mdi-wrench"></v-btn>
-      </v-toolbar>
-    </v-card>
+   <v-card width="75vw" color="secondary" height="30vh" class="d-flex align-center flex-column ml-8" elevation="12">   
+    <v-toolbar
+      class="text-medium-emphasis font-weight-light"
+      color="secondary"
+      :title="question"
+    ></v-toolbar>  
+  </v-card>
   </template>
    
-  <script>
-  export default {
-  name: 'QuizCard',
+<script>
+export default {
   props: {
-      quiz: Object,
+    question: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: Number,
+      required: true,
+    },
+    answerRating: {
+      type: Number,
+      required: true,
+    },
+    answers: {
+      type: String,
+      required: false,
+    },
   },
-  };
-  </script>
+}
+</script>
