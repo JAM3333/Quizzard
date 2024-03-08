3bd5f15b4ebc30b9cd12c0d2a04b7487aea35f85
KptnFishy <nils.miessgang@gmail.com>
2024-03-08 11:14:59 +0100

    Update loginScreen.vue

--- Quizzard/src/views/loginScreen.vue
+++ Quizzard/src/views/loginScreen.vue
@@ -1,54 +1,92 @@
 <template>
 <v-app>
-    <SideBar></SideBar>
     <v-main color="background" class="d-flex align-center justify-center ga-8 mt-20 flex-column" height="100vh">
-            <v-card width="34vw" color="secondary" height="40vh" class="d-flex align-center flex-column mr-8" elevation="12">
+            <v-card width="34vw" color="secondary" height="400px" class="d-flex align-center flex-column mr-8" elevation="12">
                 <v-btn-toggle v-model="toggle_one" mandatory width="34vw">
                     <v-btn @click="pageToLogin()" width="17vw">Login</v-btn>
                     <v-btn @click="pageToSignup()" width="17vw">Sign Up</v-btn>
                 </v-btn-toggle> 
+
+                <!-- LOGIN -->
+                <form v-if="currentPageIsLogin" ref="form" @submit.prevent="login()" style="margin-top: 60px;">
+                  <v-text-field
+                  v-if="currentLoginMethodUsername"
+                     v-model="username"
+                     name="username"
+                     label="Username"
+                     type="text"
+                     placeholder="username"
+                     required
+                  ></v-text-field>
+                  <v-text-field
+                  v-if="!currentLoginMethodUsername"
+                     v-model="email"
+                     name="email"
+                     label="eMail"
+                     type="email"
+                     placeholder="username"
+                     required
+                  ></v-text-field>
+                  <p @click="toggleEmailLogin()" v-if="currentLoginMethodUsername">login with email instead</p>
+                  <p @click="toggleEmailLogin()" v-if="!currentLoginMethodUsername">login with username instead</p>
+
+                  <v-text-field
+                     v-model="password"
+                     name="password"
+                     label="Password"
+                     type="password"
+                     placeholder="password"
+                     required
+                  ></v-text-field>
+                  <v-btn type="submit" class="mt-4" color="primary" value="log in" width="20vw">Login</v-btn>
+               </form>
+
+               <!-- SIGNUP -->
+               <form v-if="currentPageIsSignup" ref="form" @submit.prevent="signup()" style="margin-top: 35px;">
+                  <v-text-field 
+                     v-model="email"
+                     name="email"
+                     label="eMail"
+                     type="email"
+                     placeholder="email"
+                     required
+                  ></v-text-field>
+
+                  <v-text-field
+                     v-model="username"
+                     name="username"
+                     label="Username"
+                     type="text"
+                     placeholder="username"
+                     required
+                  ></v-text-field>
+                  
+                  <v-text-field
+                     v-model="password"
+                     name="password"
+                     label="Password"
+                     type="password"
+                     placeholder="password"
+                     required
+                  ></v-text-field>
+                  <v-btn type="submit" class="mt-4" color="primary" value="log in" width="20vw" :to="{path: '/home'}">Sign Up</v-btn>
+               </form>
             </v-card>
     </v-main>
 </v-app>
-    <!--
-    <v-app>
-       <v-main>
-          <v-container fluid fill-height>
-             <v-layout align-center justify-center>
-                   <v-card class="elevation-12">
-                      <v-toolbar dark color="primary">
-                         <v-toolbar-title>Login form</v-toolbar-title>
-                      </v-toolbar>
-                      <v-card-text>
-                      <form ref="form" @submit.prevent="login()">
-                             <v-text-field
-                               v-model="username"
-                               name="username"
-                               label="Username"
-                               type="text"
-                               placeholder="username"
-                               required
-                            ></v-text-field>
-                            
-                             <v-text-field
-                               v-model="password"
-                               name="password"
-                               label="Password"
-                               type="password"
-                               placeholder="password"
-                               required
-                            ></v-text-field>
-                            <v-btn type="submit" class="mt-4" color="primary" value="log in">Login</v-btn>
-                       </form>
-                      </v-card-text>
-                   </v-card>
-             </v-layout>
-          </v-container>
-       </v-main>
-    </v-app>
-    -->
- </template>
+
+
+</template>
  
+<style>
+   p:hover {
+      color: #07B5FF;
+      cursor: pointer;
+   }
+   p {
+      margin-top: 0px;
+   }                    
+</style>
  <script>
  export default {
    name: "Login",
@@ -59,6 +97,10 @@
        email: "",
        currentPageIsLogin: true,
        currentPageIsSignup: false,
+       currentLoginMethodUsername: true,
+
+       toggle_one: 0,
+       
      };
    },
    methods: {
@@ -78,6 +120,9 @@
         this.currentPageIsLogin = true;
         this.currentPageIsSignup = false;
      },
+     toggleEmailLogin() {
+         this.currentLoginMethodUsername = !this.currentLoginMethodUsername;
+     }
    },
  };
  </script>
